4dd411255a9f1c4e20aa7a030c5e46b3ea067051
Sebastian Mendez <sebas.chinoir@gmail.com>
2014-05-26 00:50:33 +0200

    Can now read XML into features

--- src/Database.hs
+++ src/Database.hs
@@ -2,6 +2,7 @@
 
 module Database where
 
+import           Text.XML.HXT.Arrow.Pickle
 import           Text.XML.HXT.Core
 
 import           Features
@@ -19,3 +20,41 @@ xmlFeatures fs =
     xmlFeature (CH area volume) =
       selem "ch" [selem "area" [txt (show area)]
                  ,selem "volume" [txt (show volume)]]
+
+instance XmlPickler Features where
+  xpickle = xpFeatures
+
+instance XmlPickler Feature where
+  xpickle = xpFeature
+
+xpFeatures =
+  xpElem "features" $
+  xpWrap (Features, \(Features fs) -> fs) $
+  xpList xpFeature
+
+xpFeature :: PU Feature
+xpFeature = PU {appUnPickle = xpChoice PU {appUnPickle = xpChoice xpD2 xpA3 xpLift} xpCH xpLift}
+
+xpD2 :: PU Feature
+xpD2 =
+  xpElem "d2" $
+  xpWrap (uncurry D2, \(D2 bins hist) -> (bins, hist)) $
+  xpPair (xpAttr "bins" xpickle) (xpList $ xpElem "value" xpickle)
+
+xpA3 :: PU Feature
+xpA3 =
+  xpElem "a3" $
+  xpWrap (uncurry A3, \(A3 bins hist) -> (bins, hist)) $
+  xpPair (xpAttr "bins" xpickle) (xpList $ xpElem "value" xpickle)
+
+xpCH :: PU Feature
+xpCH =
+  xpElem "ch" $
+  xpWrap (uncurry CH, \(CH area vol) -> (area, vol)) $
+  xpPair (xpElem "area" xpickle) (xpElem "volume" xpickle)
+
+instance XmlPickler Double where
+  xpickle = xpPrim
+
+readXmlFeatures :: XmlTree -> Maybe Features
+readXmlFeatures = unpickleDoc xpFeatures
